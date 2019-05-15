bb9ef865e752185e0fd68839c772a0e2c40d8437
Kelly <kellyandstone@gmail.com>
2019-05-15 14:50:55 +1200

    base with req.pramas:id or a?b

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # Nodejs-Express
+
+To read:
+API reference: http://expressjs.com/en/4x/api.html
+guide for Routing: http://expressjs.com/en/guide/routing.html
--- server.js
+++ server.js
@@ -2,12 +2,17 @@ var express = require("express");
 
 var app = express();
 
-app.get("/", function(req, res) {
-  //res.send("this is the homepage")
-  var responseObj = {
-    name: "Hi Kelly, this is the homepage"
-  };
-  res.send(responseObj);
+// app.get("/", function(req, res) {
+//   //res.send("this is the homepage")
+//   var responseObj = {
+//     name: "Hi Kelly, this is the homepage"
+//   };
+//   res.send(responseObj);
+// });
+
+app.get("/profile/:id/user/:name", function(req, res) {
+  console.dir(req.params);
+  res.send("you are getting " + req.params.id);
 });
 
 app.listen(3001);
