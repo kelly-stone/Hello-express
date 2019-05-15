8e2d2eb6e47ead0f039a4d0b457252017ea49204
Kelly <kellyandstone@gmail.com>
2019-05-15 14:25:30 +1200

    please read expressjs.com, read the drop down from API reference 4*

--- server.js
+++ server.js
@@ -3,7 +3,11 @@ var express = require("express");
 var app = express();
 
 app.get("/", function(req, res) {
-  res.send("this is the homepage");
+  //res.send("this is the homepage")
+  var responseObj = {
+    name: "Hi Kelly, this is the homepage"
+  };
+  res.send(responseObj);
 });
 
 app.listen(3001);
