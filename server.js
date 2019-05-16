39195027b43d780186235785d74ff0f3dba3c7f6
Kelly <kellyandstone@gmail.com>
2019-05-16 15:18:45 +1200

    using body-parser

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 var express = require("express");
+var bodyParser = require("body-parser");
 
 var app = express();
+app.use(bodyParser.urlencoded({ extended: false }));
 
 // app.get("/", function(req, res) {
 //   //res.send("this is the homepage")
